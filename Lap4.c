85ae877ffd55fe6efbfa0648b8249fde51731faf
gavrilovaanna0108-dotcom <gavrilovaanna0108@gmail.com>
2026-09-23 18:05:10 +0300

    Гаврилова А.С. выполнила дз по практической 1

---
+++ Lap4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    puts("          |\\ ");
+    puts("          | \\ ");
+    puts("          |  \\ ");
+    puts("          |   \\ ");
+    puts("     _____|____\\_____ ");
+    puts("     \\              / ");
+    puts("      \\____________/");
+    return 0;
+}
